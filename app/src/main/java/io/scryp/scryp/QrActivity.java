04dbe82f744d0952c253e497038ea89ec20900fe
Matt McGivney <matt@mattmcgivney.com>
2017-07-25 13:39:20 -0600

    Add zxing intent launcher to activity...QR scanner launching successfully in emulator.

--- app/src/main/java/io/scryp/scryp/QrActivity.java
+++ app/src/main/java/io/scryp/scryp/QrActivity.java
@@ -1,11 +1,16 @@
 package io.scryp.scryp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class QrActivity extends AppCompatActivity {
 
@@ -24,6 +29,23 @@ public class QrActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        new IntentIntegrator(this).initiateScan(); // `this` is the current Activity
+
     }
 
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
