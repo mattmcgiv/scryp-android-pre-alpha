47d0fe93d4c9e9035a8001678cf7161ce5e73100
Matt McGivney <matt@mattmcgivney.com>
2017-07-31 12:00:28 -0600

    Updates to HOME btn and its click action.

--- app/src/main/java/io/scryp/scryp/TransactionCompleteActivity.java
+++ app/src/main/java/io/scryp/scryp/TransactionCompleteActivity.java
@@ -1,11 +1,13 @@
 package io.scryp.scryp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class TransactionCompleteActivity extends AppCompatActivity {
 
@@ -16,6 +18,15 @@ public class TransactionCompleteActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.paymentStatus);
         setSupportActionBar(toolbar);
+
+
+        Button homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ConfirmTransactionActivity.class);
+                v.getContext().startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
     }
 
 }
