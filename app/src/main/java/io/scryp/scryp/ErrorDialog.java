df6fe27d3129483cfae5a019403f8945f5257b6f
Matt McGivney <matt@mattmcgivney.com>
2017-08-11 12:12:39 -0600

    Add an ErrorDialog class for displaying errors.

---
+++ app/src/main/java/io/scryp/scryp/ErrorDialog.java
@@ -0,0 +1,32 @@
+package io.scryp.scryp;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by mattmcgivney on 8/10/17.
+ */
+
+public class ErrorDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Oh no! We had a problem with your request.")
+                .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
