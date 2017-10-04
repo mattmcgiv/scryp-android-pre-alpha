9892af5a5d0b4ad5aae802d35b2e18b2c085710d
Matt McGivney <matt@mattmcgivney.com>
2017-10-03 23:47:07 -0600

    Update resources.

--- app/src/androidTest/java/io/scryp/scryp/QRUtilitiesTest.java
+++ app/src/androidTest/java/io/scryp/scryp/QRUtilitiesTest.java
@@ -4,7 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static io.scryp.scryp.QRUtilities.isQRFormattedCorrectly;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Matt on 8/24/2017.
